515e9892dd0a2d1b42c239bf6d6c79bd9b80a1e8
Ahmed Musa Awon <ahmed.musa.awon@welldev.io>
2021-02-03 09:16:37 +0600

    Fixed Indentations

--- app.rb
+++ app.rb
@@ -16,25 +16,25 @@ found = false
 count = 0
 
 while flag && !found
-    if count == 3
-        puts "Maximum Attempt Limit Reached"
-        break
-    else
-        print "username : "
-        username = gets.chomp
-        print "password : "
-        password = gets.chomp
-        users.each do |user|
-            if user[:username] == username
-                if user[:password] == password
-                    puts user
-                    found = true
-                    break
-                else
-                    puts "Wrong Password"
-                end
-                count = count + 1
-            end
+  if count == 3
+    puts "Maximum Attempt Limit Reached"
+    break
+  else
+    print "username : "
+    username = gets.chomp
+    print "password : "
+    password = gets.chomp
+    users.each do |user|
+      if user[:username] == username
+        if user[:password] == password
+          puts user
+          found = true
+          break
+        else
+          puts "Wrong Password"
         end
+        count = count + 1
+      end
     end
+  end
 end
